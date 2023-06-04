a8917f2d9703d0f53dcc4c9a565b570d6bc633c5
ritesh-1997 <riteshg763@gmail.com>
2023-06-04 09:54:52 +0530

    Adding Queue/Dota2

---
+++ Queue/Dota.cpp
@@ -0,0 +1,51 @@
+/**
+ * In the world of Dota2, there are two parties: the Radiant and the Dire.
+ *
+ * The Dota2 senate consists of senators coming from two parties.
+ * Now the Senate wants to decide on a change in the Dota2 game.
+ * The voting for this change is a round-based procedure. In each round,
+ * each senator can exercise one of the two rights:
+ *
+ * Ban one senator's right: A senator can make another senator lose all his
+ * rights in this and all the following rounds.
+ *
+ * Announce the victory: If this senator found the senators who still
+ * have rights to vote are all from the same party, he can announce the
+ * victory and decide on the change in the game.
+ *
+ * Given a string senate representing each senator's party belonging.
+ * The character 'R' and 'D' represent the Radiant party and the Dire party.
+ * Then if there are n senators, the size of the given string will be n.
+ *
+ * The round-based procedure starts from the first senator to the last senator
+ * in the given order. This procedure will last until the end of voting.
+ * All the senators who have lost their rights will be skipped during the procedure.
+ *
+ * Suppose every senator is smart enough and will play the best strategy for his own party.
+ *
+ * Predict which party will finally announce the victory and change the Dota2 game.
+ * The output should be "Radiant" or "Dire".
+ *
+ */
+
+class Solution
+{
+public:
+    string predictPartyVictory(string senate)
+    {
+        queue<int> r, d;
+        int i = 0, n = senate.size();
+        for (auto i = 0; i < n; i++)
+        {
+            (senate[i] == 'R') ? r.push(i) : d.push(i);
+        }
+        while (!r.empty() && !d.empty())
+        {
+            int r_index = r.front();
+            int d_index = d.front();
+            r.pop(), d.pop();
+            (r_index < d_index) ? r.push(r_index + n) : d.push(d_index + n);
+        }
+        return (r.size() > d.size()) ? "Radiant" : "Dire";
+    }
+};
