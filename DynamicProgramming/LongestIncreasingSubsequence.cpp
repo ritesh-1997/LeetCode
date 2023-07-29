7474dfd74c15a49ce200c142fb9628575cb442d3
ritesh-1997 <riteshg763@gmail.com>
2023-07-29 16:05:31 +0530

    updating code

--- DynamicProgramming/LongestIncreasingSubsequence.cpp
+++ DynamicProgramming/LongestIncreasingSubsequence.cpp
@@ -1,16 +1,25 @@
-class Solution {
+/**
+ * Given an integer array nums, return the length of the longest strictly increasing
+ * subsequence
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ */
+class Solution
+{
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int dp[n],res = 1;
-        memset(dp,1,sizeof(dp));
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size(), res = 1;
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; i++)
+        {
 
-        for(int i=0;i<n;i++){
-            dp[i] = 1;
-            for(int j = 0;j<=i;j++){
-                if(nums[i]>nums[j])
-                dp[i] = max(dp[i],dp[j]+1);
-                res = max(res,dp[i]);
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                    dp[i] = max(dp[i], dp[j] + 1);
+                res = max(dp[i], res);
             }
         }
         return res;
