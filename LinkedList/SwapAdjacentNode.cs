6fce7e95955759a298dd3763b015cc706e245710
ritesh-1997 <riteshg763@gmail.com>
2023-08-02 23:33:45 +0530

    updatin .cs file

--- LinkedList/SwapAdjacentNode.cs
+++ LinkedList/SwapAdjacentNode.cs
@@ -9,6 +9,28 @@
  *     }
  * }
  */
+
+ /**
+ Given a linked list, swap every two adjacent nodes and return its head. 
+ You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example Input
+ Input 1: 
+
+ A = 1 -> 2 -> 3 -> 4
+ Input 2: 
+
+ A = 7 -> 2 -> 1
+
+
+Example Output
+ Output 1: 
+
+ 2 -> 1 -> 4 -> 3
+ Output 2: 
+
+ 2 -> 7 -> 1
+ */
 public class Solution {
     public ListNode SwapPairs(ListNode head) {
         ListNode slow = head,fast = head;
